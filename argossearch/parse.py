fc70ac19bf9be6c1683216e5549cd21b4bbef9d8
P.J. Finlay <pjappdevelopment@gmail.com>
2021-10-26 19:49:17 -0500

    Added Link

--- argossearch/parse.py
+++ argossearch/parse.py
@@ -12,13 +12,25 @@ def network_get(url):
     response = request.urlopen(req)
     return response.read().decode()
 
+class Link:
+    def __init__(self, bs):
+        self.bs = bs
+        self.text = get_text(bs)
+        self.url = self.bs['href']
+
+    def __str__(self):
+        return f'[{self.text}]({self.url})'
+
 class Page:
     def __init__(self, url):
         self.url = url
         self.raw_text = network_get(self.url)
         self.soup = BeautifulSoup(self.raw_text, features="html.parser")
         self.text = get_text(self.soup)
-        print(self.text)
+        self.links = [Link(link) for link in self.soup.find_all('a')]
 
 
 page = Page("https://www.argosopentech.com")
+for link in page.links:
+    print(link)
+
